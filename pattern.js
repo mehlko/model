e1051483ef640cc649095f8b39fc5687909266ce
mehlko <mehl@uni-koblenz.de>
2021-09-21 12:35:06 +0200

    2 files modified

--- pattern.js
+++ pattern.js
@@ -8,18 +8,29 @@ var patternList = {
        ?process model:hasOutputProduct ?product .
        ?process model:hasNextProcess ?nextProcess .
        MINUS {?nextProcess model:hasInputProduct ?product .}
-      } LIMIT 10`,
+      }`,
+
     affectedElements: (queryResult) => {
       return [
         queryResult.get('?process').value,
         queryResult.get('?product').value,
+        queryResult.get('?nextProcess').value,
       ];
     },
+
     reason: (queryResult) => {
       return (
+        'Input Output Mismatch is detected between process  ' +
         queryResult.get('?process').value +
-        ' ' +
-        queryResult.get('?product').value
+        ' and ' +
+        queryResult.get('?nextProcess').value +
+        '.' +
+        'Product ' +
+        queryResult.get('?product').value +
+        ' is produced by ' +
+        queryResult.get('?process').value +
+        ' but not acccepted by ' +
+        queryResult.get('?nextProcess').value
       );
     },
   },
