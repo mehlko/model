4bf20ad1e4f0066e8091c91855cfb9d2570e6714
mehlko <mehl@uni-koblenz.de>
2021-09-23 15:09:23 +0200

    1 file modified

--- pattern.js
+++ pattern.js
@@ -38,7 +38,7 @@ var patternList = {
   productExposedToRiskSource: {
     name: 'productExposedToRiskSource',
     description: '',
-    queryString: `SELECT * WHERE {a b c.}`,
+    queryString: `SELECT * WHERE { ?a b c.}`,
 
     affectedElements: (queryResult) => {
       return [];
